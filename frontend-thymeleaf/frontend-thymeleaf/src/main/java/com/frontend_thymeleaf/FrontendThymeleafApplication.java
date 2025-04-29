69d80ed8964ca5a0f1796e0808aaa4c1d26c024f
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-04-28 21:43:30 -0300

    feat: create a frontend begin structure

---
+++ frontend-thymeleaf/frontend-thymeleaf/src/main/java/com/frontend_thymeleaf/FrontendThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.frontend_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendThymeleafApplication.class, args);
+	}
+
+}
